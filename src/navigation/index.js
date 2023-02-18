921fdb495faced945461848032e770a959d332ab
Bharati Subramanian <bharatisharada@gmail.com>
2023-02-18 10:09:52 +0530

    feat: navigation

---
+++ src/navigation/index.js
@@ -0,0 +1,33 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import EditCustomer from "../features/Customer/Edit";
+import CustomerList from "../features/Customer/List";
+import CreateCustomer from "../features/Customer/New";
+import RegionsList from "../features/Regions/List";
+import Welcome from "../screens/Welcome";
+
+const Navigation = () => {
+	const Stack = createNativeStackNavigator();
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="Welcome"
+				screenOptions={{
+					headerTitleAlign: "center",
+				}}
+			>
+				<Stack.Screen name="Welcome" component={Welcome} />
+				<Stack.Screen name="RegionsList" component={RegionsList} />
+				<Stack.Screen
+					name="CreateCustomer"
+					component={CreateCustomer}
+				/>
+				<Stack.Screen name="CustomersList" component={CustomerList} />
+				<Stack.Screen name="EditCustomer" component={EditCustomer} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default Navigation;
