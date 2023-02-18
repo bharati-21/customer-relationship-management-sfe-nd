648fde259ebb3908701a0db88673a1345d94a2c2
Bharati Subramanian <bharatisharada@gmail.com>
2023-02-18 18:58:09 +0530

    refactor: container wrapper

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,11 +1,15 @@
 import { ScrollView, View } from "react-native";
 import { containerStyles } from "./styles";
 
-const Container = ({ children }) => {
+const Container = ({ children, enableScroll = true }) => {
 	const styles = containerStyles();
-	return <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.container}>{children}</View>
-    </ScrollView>;
+	return !enableScroll ? (
+		<View style={styles.container}>{children}</View>
+	) : (
+		<ScrollView contentContainerStyle={styles.scrollView}>
+			<View style={styles.container}>{children}</View>
+		</ScrollView>
+	);
 };
 
 export default Container;
