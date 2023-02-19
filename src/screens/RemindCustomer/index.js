a6d221562ddb3d398a56f4bf2db8991af907cb27
Bharati Subramanian <bharatisharada@gmail.com>
2023-02-19 21:05:31 +0530

    feat: remind customer screen with notification

---
+++ src/screens/RemindCustomer/index.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+import { Keyboard, Text } from "react-native";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import { useSelector } from "react-redux";
+import Button from "../../components/Button";
+import Container from "../../components/Container";
+import { remindCustomerStyles } from "./style";
+
+const customer = {
+	firstName: "Jane",
+	lastName: "Doe",
+	region: "mumbai",
+	status: "INACTIVE",
+};
+
+const onSubmit = (randomCustomer, seconds) => {
+	Keyboard.dismiss();
+	const schedulingOptions = {
+		content: {
+			title: `Don't forget to contact ${randomCustomer.firstName} ${randomCustomer.lastName}`,
+			body: "Open the app to contact the customer and get constant feedback!",
+			sound: true,
+			priority: Notifications.AndroidNotificationPriority.HIGH,
+			color: "blue",
+		},
+		trigger: {
+			seconds: seconds,
+		},
+	};
+	Notifications.scheduleNotificationAsync(schedulingOptions)
+		.then(() => console.log("Displayed push notification successfully!"))
+		.catch((error) => {
+			console.log(error.toString());
+		});
+};
+
+const handleNotification = () => {
+	console.warn("Your notification ran, but won`t show up in the app!");
+};
+
+const askNotification = async () => {
+	const { status } = await Notifications.requestPermissionsAsync();
+	if (Constants.isDevice && status === "granted") {
+		console.log("Notification permissions granted.");
+	}
+};
+
+const getRandomIndex = (length) => Math.floor(Math.random() * length);
+
+const RemindCustomer = () => {
+	const customers = useSelector((state) => state.customer.list.customers);
+	const randomCustomer =
+		customers.length > 0
+			? customers[getRandomIndex(customer.length)]
+			: customer;
+
+	useEffect(() => {
+		askNotification();
+
+		const listener =
+			Notifications.addNotificationReceivedListener(handleNotification);
+		return () => listener.remove();
+	}, []);
+
+	const styles = remindCustomerStyles();
+
+	return (
+		<Container>
+			<Text style={styles.heading}>
+				Press the button to get your reminder to contact customer!
+			</Text>
+
+			<Button
+				onPressHandler={() => onSubmit(randomCustomer, 5)}
+				buttonText="Schedule"
+			/>
+		</Container>
+	);
+};
+
+export default RemindCustomer;
---
+++ src/screens/RemindCustomer/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const remindCustomerStyles = () =>
+	StyleSheet.create({
+		heading: {
+			fontSize: 24,
+			fontWeight: "bold",
+			alignSelf: "center",
+			textAlign: "center",
+		},
+	});
+
+export { remindCustomerStyles };
