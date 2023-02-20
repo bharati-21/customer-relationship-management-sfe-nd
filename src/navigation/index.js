e37868dc5fc8738b1af761093f275c02819d15b2
Bharati Subramanian <bharatisharada@gmail.com>
2023-02-20 21:14:03 +0530

    refactor: screen component

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import EditCustomer from "../features/Customer/Edit";
-import CustomerList from "../features/Customer/List";
-import CreateCustomer from "../features/Customer/New";
-import RemindCustomer from "../features/Customer/Remind";
-import RegionsList from "../features/Regions/List";
+import CreateCustomerScreen from "../screens/CreateCustomerScreen";
+import CustomersListScreen from "../screens/CustomersListScreen";
+import EditCustomerScreen from "../screens/EditCustomerScreen";
+import RegionsListScreen from "../screens/RegionsListScreen";
+import RemindCustomerScreen from "../screens/RemindCustomerScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import { navigationRef } from "./NavigationService";
 
@@ -20,16 +20,25 @@ const Navigation = () => {
 				}}
 			>
 				<Stack.Screen name="Welcome" component={WelcomeScreen} />
-				<Stack.Screen name="RegionsList" component={RegionsList} />
+				<Stack.Screen
+					name="RegionsList"
+					component={RegionsListScreen}
+				/>
 				<Stack.Screen
 					name="CreateCustomer"
-					component={CreateCustomer}
+					component={CreateCustomerScreen}
 				/>
-				<Stack.Screen name="CustomersList" component={CustomerList} />
-				<Stack.Screen name="EditCustomer" component={EditCustomer} />
+				<Stack.Screen
+					name="CustomersList"
+					component={CustomersListScreen}
+				/>
+				<Stack.Screen
+					name="EditCustomer"
+					component={EditCustomerScreen}
+				/>
 				<Stack.Screen
 					name="RemindCustomer"
-					component={RemindCustomer}
+					component={RemindCustomerScreen}
 				/>
 			</Stack.Navigator>
 		</NavigationContainer>
